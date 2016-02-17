996e2187436bd77fd36cd9f17a7f22823b637371
Ugis Brekis <ugis.brekis@hotmail.lv>
2016-02-17 01:04:09 +0000

    Now uses Path2D Node as a base

--- Sidescroller_ground_edit/Scripts/ground_edit.gd
+++ Sidescroller_ground_edit/Scripts/ground_edit.gd
@@ -1,109 +1,69 @@
 tool
 
-extends Node2D
+extends Path2D
 
-var curve
-var selectedID = null
-var pointIn = null # true=pointIn ; false=pointOut
+export(Texture) var texture = preload("res://Sprites/ground.png") setget setBakeInterval
+export(bool) var debug = true setget setDebug
 
-export var bakeInterval = 64
-export(Texture) var texture = preload("res://Sprites/ground.png")
+var bakeInterval = 64
 
-export(bool) var showInfo = true
+
 
 
 func _ready():
-	curve = Curve2D.new()
+	get_curve().connect("changed", self, "updateCurve")
 	
-	curve.add_point(Vector2(50, 50), Vector2(-50, 0), Vector2(500, 0))
-	curve.add_point(get_viewport_rect().size - Vector2(50, 50), Vector2(-500, 0), Vector2(50, 0))
 	
-	curve.set_bake_interval(bakeInterval)
-	
+func updateCurve():
 	update()
 	
-	set_process_input(true)
+func setDebug(val):
+	debug = val
+	update()
 	
 	
-func _input(event):
-	if event.type == InputEvent.MOUSE_BUTTON:
-		if event.pressed:
-			for p in range(curve.get_point_count()):
-				if event.pos.distance_to(curve.get_point_pos(p)) < 10:
-					selectedID = p
-					pointIn = null
-					break
-				elif event.pos.distance_to(curve.get_point_pos(p) + curve.get_point_in(p)) < 10:
-					selectedID = p
-					pointIn = true
-					break
-				elif event.pos.distance_to(curve.get_point_pos(p) + curve.get_point_out(p)) < 10:
-					selectedID = p
-					pointIn = false
-					break
-				else:
-					selectedID = null
-					pointIn = null
-		else:
-			selectedID = null
-			pointIn = null
-		update()
-		
-	if event.type == InputEvent.MOUSE_MOTION:
-		if selectedID != null:
-			if pointIn == null:
-				curve.set_point_pos(selectedID, event.pos)
-				update()
-			elif pointIn == true:
-				curve.set_point_in(selectedID, event.pos - curve.get_point_pos(selectedID))
-				update()
-			elif pointIn == false:
-				curve.set_point_out(selectedID, event.pos - curve.get_point_pos(selectedID))
-				update()
 	
 func _draw():
 	#Normals
 	var normals = []
 	
-	for p in range(curve.get_baked_points().size()):
+	for p in range(get_curve().get_baked_points().size()):
 		var n = Vector2()
 		
 		if p == 0:
-			var a = (curve.get_point_pos(0) - curve.get_baked_points()[p]).normalized()
-			var b = (curve.get_baked_points()[p+1] - curve.get_baked_points()[p]).normalized()
+			var a = (get_curve().get_point_pos(0) - get_curve().get_baked_points()[p]).normalized()
+			var b = (get_curve().get_baked_points()[p+1] - get_curve().get_baked_points()[p]).normalized()
 			
-			n = (a+b).normalized()*bakeInterval
+			#Fix normal direction
+			if get_curve().get_baked_points()[p].y + n.y > get_curve().get_baked_points()[p].y:
+				n = -n
 			
-			#Make normals face up
-			if curve.get_baked_points()[p].y + n.y > curve.get_baked_points()[p].y:
+			n = (a+b).normalized()*bakeInterval/2
+			normals.push_back(n)
+			
+		elif p > 0 && p < get_curve().get_baked_points().size()-1:
+			var a = (get_curve().get_baked_points()[p-1] - get_curve().get_baked_points()[p]).normalized()
+			var b = (get_curve().get_baked_points()[p+1] - get_curve().get_baked_points()[p]).normalized()
+			
+			n = (a+b).normalized()*bakeInterval/2
+			
+			#Fix normal direction
+			if get_curve().get_baked_points()[p].y + n.y > get_curve().get_baked_points()[p].y:
 				n = -n
 				
 			normals.push_back(n)
-		elif p > 0 && p < curve.get_baked_points().size()-1:
-			var a = (curve.get_baked_points()[p-1] - curve.get_baked_points()[p]).normalized()
-			var b = (curve.get_baked_points()[p+1] - curve.get_baked_points()[p]).normalized()
 			
-			n = (a+b).normalized()*bakeInterval
-			
-			#Make normals face up
-			if curve.get_baked_points()[p].y + n.y > curve.get_baked_points()[p].y:
-				n = -n
-				
-			normals.push_back(n)
-			
-		
-		
-		
+	
 	#Polygon drawing
 	var polyArray = []
 	var uvArray = []
 	
 	for p in range(normals.size()-1):
 		
-		var p0 = curve.get_baked_points()[p] + normals[p]
-		var p1 = curve.get_baked_points()[p+1] + normals[p+1]
-		var p2 = curve.get_baked_points()[p+1] - normals[p+1]
-		var p3 = curve.get_baked_points()[p] - normals[p]
+		var p0 = get_curve().get_baked_points()[p] + normals[p]
+		var p1 = get_curve().get_baked_points()[p+1] + normals[p+1]
+		var p2 = get_curve().get_baked_points()[p+1] - normals[p+1]
+		var p3 = get_curve().get_baked_points()[p] - normals[p]
 		
 		polyArray.push_back([p0, p1, p2, p3])
 
@@ -117,42 +77,24 @@ func _draw():
 	
 	for p in range(normals.size()-1):
 		draw_polygon(polyArray[p], colArray, uvArray, texture)
-		
-	if showInfo:
+	
+	#Draws lines only when debug enabled
+	if debug:
 		#Draw curve
 		#Start with the first point
-		if curve.get_baked_points().size() > 0:
-			draw_line(curve.get_point_pos(0), curve.get_baked_points()[0], Color(0, 0.5, 1), 1)
-		
+		if get_curve().get_baked_points().size() > 0:
+			draw_line(get_curve().get_point_pos(0), get_curve().get_baked_points()[0], Color(0, 0.5, 1), 1)
+			
 		#Continue with drawing the whole curve
-		for p in range(curve.get_baked_points().size()):
-			if p < curve.get_baked_points().size() -1:
-				draw_line(curve.get_baked_points()[p], curve.get_baked_points()[p+1], Color(0, 0.5, 1), 1)
-		
-		
+		for p in range(get_curve().get_baked_points().size()):
+			if p < get_curve().get_baked_points().size() -1:
+				draw_line(get_curve().get_baked_points()[p], get_curve().get_baked_points()[p+1], Color(0, 0.5, 1), 1)
+				
 		for n in range(normals.size()):
-			draw_line(curve.get_baked_points()[n], curve.get_baked_points()[n] - normals[n], Color(0, 1, 0), 1)
+			draw_line(get_curve().get_baked_points()[n], get_curve().get_baked_points()[n] - normals[n], Color(0, 1, 0), 1)
 	
 	
-	
-	if selectedID != null:
-		if pointIn == true:
-			draw_circle(curve.get_point_pos(selectedID) + curve.get_point_in(selectedID), 6, Color(1, 1, 1, 0.25))
-		elif pointIn == false:
-			draw_circle(curve.get_point_pos(selectedID) + curve.get_point_out(selectedID), 6, Color(1, 1, 1, 0.25))
-		else:
-			draw_circle(curve.get_point_pos(selectedID), 6, Color(0, 1, 0, 0.25))
-		
-		
-		
-	for p in range(curve.get_point_count()):
-		var a = curve.get_point_in(p)
-		var b = curve.get_point_out(p)
-		
-		draw_line(curve.get_point_pos(p), curve.get_point_pos(p) + a, Color(1, 0.5, 0))
-		draw_line(curve.get_point_pos(p), curve.get_point_pos(p) + b, Color(1, 0, 0.5))
-		
-		draw_circle(curve.get_point_pos(p) + a, 3, Color(1, 0.5, 0))
-		draw_circle(curve.get_point_pos(p) + b, 3, Color(1, 0, 0.5))
-		
-		draw_circle(curve.get_point_pos(p), 3, Color(0, 0.5, 1))
+func setBakeInterval(val):
+	texture = val
+	bakeInterval = texture.get_width()
+
